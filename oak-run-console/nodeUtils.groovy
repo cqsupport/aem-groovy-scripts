ed35e93d828cf147aa4c26685b5d7b566300eea5
Andrew Khoury <andrewmkhoury@gmail.com>
2021-01-06 11:20:16 -0800

    Update nodeUtils.groovy

--- oak-run-console/nodeUtils.groovy
+++ oak-run-console/nodeUtils.groovy
@@ -41,3 +41,24 @@ def renameNode(def session, String path, String newName) {
         return false
     }
 }
+
+def addChildNode(def session, String path, String newChildNodeName, String primaryType) {
+    println "Renaming node from ${path} to ${newName}"
+    
+    NodeStore ns = session.store
+    def nb = ns.root.builder()
+
+    def aBuilder = nb
+    def parentNodeBuilder = nb
+    for(p in PathUtils.elements(path)) {  aBuilder = aBuilder.getChildNode(p); }
+
+    if(aBuilder.exists()) {
+        newNodeBuilder = aBuilder.setChildNode(newChildNodeName);
+        newNodeBuilder.setProperty("jcr:primaryType", org.apache.jackrabbit.oak.api.Type.NAME, primaryType);
+        ns.merge(nb, EmptyHook.INSTANCE, CommitInfo.EMPTY)
+        return rn
+    } else {
+        println "Parent node ${path} doesn't exist"
+        return false
+    }
+}
