79ef66f947cb52c6a36b3050f53acd7503993eb5
John Bunker <johnwbunker@gmail.com>
2022-08-29 15:19:55 -0700

    Update BlobID___Asset_in_AEM_Script.groovy

--- BlobID___Asset_in_AEM_Script.groovy
+++ BlobID___Asset_in_AEM_Script.groovy
@@ -3,6 +3,11 @@
 // See: https://gist.github.com/andrewmkhoury/080d9e53dfed5115b90df9f5b06521fe
 // This version works with Oak 1.6 and later versions
 // 
+// Can be run when AEM is running or when shutdown using oak-run:
+//     $ java -jar oak-run*.jar console crx-quickstart/repository/segmentstore ":load BlobID___Asset_in_AEM_Script.groovy"
+// Run using oak-run by removing comment at the end of the file:
+//     countNodes(session.workingNode)
+
 // Runs in on-line Groovy console http://localhost:4502/system/console/sc
 // http://felix.apache.org/documentation/subprojects/apache-felix-script-console-plugin.html
 // 1. Web Console Script Console Plugin (which enabled the console at /system/console/sc)
@@ -16,20 +21,22 @@ import org.apache.jackrabbit.oak.spi.state.NodeStore
 
 // Prints out the path and blob ID for all non-null blob IDs under the path defined by "traversalPath".
 // Optionally, if you provide a list of blob IDs in "matchingBlobIds" it will only print out the path for matching blob IDs.
-// Such as: String[] matchingBlobIds = ["a6cab9d7e3829d90ff2456872dbddad1e313df0c56e089f562f34a6a6ddbabe4#22054"]
+// Such as: String[] matchingBlobIds = ["a6cab9d7e3829d90ff2456872dbddad1e313df0c56e089f562f34a6a6ddbabe4"]
+// BlobId can be suffixed with the "#" value, such as "bd5d6b043e96622aba2079d4912d32d3ff217b2ec04b216502ad17e9db544101#23404"
 
-def countNodes(NodeState n, deep = false, String path = "/", Integer flush = 100000, AtomicInteger count = new AtomicInteger(0), 
-               AtomicInteger binaries = new AtomicInteger(0), root = true, 
-               String traversalPath = "/content", 
-               String[] matchingBlobIds = [])
-{
+def countNodes(NodeState n, String path = "/", Integer flush = 100000, AtomicInteger count = new AtomicInteger(0), 
+               AtomicInteger binaries = new AtomicInteger(0), root = true, String traversalPath = "/", 
+               String[] matchingBlobIds = [
+                 "fff602a16739a32df02a883a57cfc88786f9a6d80dd0623d9b2e9311312ddb7d",
+                 "ff0ef7e31728a020f5f28962a503e2715d4170360d15c422fdaa141d88bc2b53",
+		 "bd5d6b043e96622aba2079d4912d32d3ff217b2ec04b216502ad17e9db544101#23404"]) {
   if (root) 
   {
-    out.println "Counting nodes in tree ${path}"
+    println "Counting nodes in tree ${path}"
   }
 
   cnt = count.incrementAndGet()
-  if (cnt % flush == 0) out.println("  " + cnt)
+  if (cnt % flush == 0) println("  " + cnt)
 
   try 
   {
@@ -43,30 +50,40 @@ def countNodes(NodeState n, deep = false, String path = "/", Integer flush = 100
           {
             for (b in prop.getValue(Type.BINARIES)) 
             {
-              if (   (null != b.getBlobId()) 
-                  && ((0 == matchingBlobIds.size()) || (matchingBlobIds.contains(b.getBlobId()))))
+              if (null != b.getBlobId())
               {
-                out.println "${path} ====> ${b.getBlobId()}"
-              }
+		def blobId = b.getBlobId().split("#")[0]
+                if (   (0 == matchingBlobIds.size()) 
+                    || matchingBlobIds.contains(blobId) 
+                    || matchingBlobIds.contains(b.getBlobId()))
+		{
+		  println "${path} ==> ${b.getBlobId()}"
+		}
+	      }
               binaries.incrementAndGet();
             }
           } 
           else if (prop.getType() == Type.BINARY) 
           {
             def b = prop.getValue(Type.BINARY);
-            if (   (null != b.getBlobId())
-                && ((0 == matchingBlobIds.size()) || (matchingBlobIds.contains(b.getBlobId()))))
-            { 
-              out.println "${path} ====> ${b.getBlobId()}"
-            }
-            binaries.incrementAndGet();
+            if (null != b.getBlobId())
+	    {
+              def blobId = b.getBlobId().split("#")[0]
+              if (   (0 == matchingBlobIds.size()) 
+                  || matchingBlobIds.contains(blobId) 
+                  || matchingBlobIds.contains(b.getBlobId()))
+              { 
+		println "${path} ==> ${b.getBlobId()}"
+              }
+              binaries.incrementAndGet();
+	    }
           }
         }
       }
     } 
     catch (e) 
     {
-      out.println "warning unable to read node properties ${path} : " + e.getMessage()
+      println "warning unable to read node properties ${path} : " + e.getMessage()
       org.codehaus.groovy.runtime.StackTraceUtils.printSanitizedStackTrace(e, out)
     }
     try 
@@ -75,58 +92,57 @@ def countNodes(NodeState n, deep = false, String path = "/", Integer flush = 100
       {
         try 
         {
-          countNodes(child.getNodeState(), deep, path + child.getName() + "/", flush, count, binaries, false, traversalPath, matchingBlobIds)
+          countNodes(child.getNodeState(), path + child.getName() + "/", flush, count, binaries, false, traversalPath, matchingBlobIds)
         } 
         catch (e) 
         {
-          out.println "warning unable to read child node ${path} : " + e.getMessage()
-	      org.codehaus.groovy.runtime.StackTraceUtils.printSanitizedStackTrace(e, out)
+          println "warning unable to read child node ${path} : " + e.getMessage()
+          org.codehaus.groovy.runtime.StackTraceUtils.printSanitizedStackTrace(e, out)
         }
       }
     } 
     catch (e) 
     {
-      out.println "warning unable to read child entries ${path} : " + e.getMessage()
+      println "warning unable to read child entries ${path} : " + e.getMessage()
       org.codehaus.groovy.runtime.StackTraceUtils.printSanitizedStackTrace(e, out)
     }
   } 
   catch(e) 
   {
-    out.println "warning unable to read node ${path} : " + e.getMessage()
+    println "warning unable to read node ${path} : " + e.getMessage()
     org.codehaus.groovy.runtime.StackTraceUtils.printSanitizedStackTrace(e, out)
   }
 
   if (root) 
   {
-    out.println "Total nodes in tree ${path}: ${cnt}"
-    out.println "Total binaries in tree ${path}: ${binaries.get()}"
+    println "Total nodes in tree ${path}: ${cnt}"
+    println "Total binaries in tree ${path}: ${binaries.get()}"
   }
 
   return cnt
 }
 
-def countNodes(session) 
-{
+/*****************************  For running in script console (/system/console/sc):
+******************************/
+def countNodes(session) {
     NodeStore nstore = session.getRootNode().sessionDelegate.root.store
-    def rs = nstore.root
-    def ns = rs
-    out.println("Running node counter")
+    def ns = nstore.root
+    println("Running node counter")
     countNodes(ns)
-    out.println("Done")
+    println("Done")
 }
-
-def countNodes() 
-{
-	def repo = osgi.getService(org.apache.sling.jcr.api.SlingRepository)
-	def session = repo.loginAdministrative(null)
-	try 
-    {
-	    countNodes(session)
-	} 
-    finally 
-    {
-	    session.logout()
-	}
+def countNodes() {
+    def repo = osgi.getService(org.apache.sling.jcr.api.SlingRepository)
+    def session = repo.loginAdministrative(null)
+    try {
+        countNodes(session)
+    } finally {
+        session.logout()
+    }
 }
-
 countNodes()
+
+
+/****************** For running using oak-run: 
+countNodes(session.workingNode)
+*******************/
